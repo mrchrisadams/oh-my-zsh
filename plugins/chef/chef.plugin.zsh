fc3a85ee6345c744726da699fbf0d18f4b232742
Chris Adams <wave@chrisadams.me.uk>
2011-06-22 09:01:43 +0100

    Pulled out local changes into plugins local dirs

---
+++ plugins/chef/chef.plugin.zsh
@@ -0,0 +1,4 @@
+
+# Knife will often be intercepted by zsh's attempted corrections of filenames
+alias knife='nocorrect knife'
+
---
+++ plugins/wget/wget.plugin.zsh
@@ -0,0 +1,4 @@
+# If you're debugging http connections, this alias is incredibly handy
+#
+alias wget-head='wget --server-response --spider'
+
