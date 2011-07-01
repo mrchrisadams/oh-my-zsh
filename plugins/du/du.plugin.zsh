46d97fcfbb188a733567b03d6c810d21104fad5c
Chris Adams <wave@chrisadams.me.uk>
2011-07-01 01:56:13 +0100

    Handy disk usage command line alias

---
+++ plugins/du/du.plugin.zsh
@@ -0,0 +1,5 @@
+# I first saw Loren Segal using this on the # Yard forums,
+# and knew it'd be worth putting into a zsh plugin
+# http://groups.google.com/group/yardoc/msg/00836e6bd308b242
+# works on mac os x, and linux
+alias duksh='du -ksh'
